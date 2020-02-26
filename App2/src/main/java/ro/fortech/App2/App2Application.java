79d0be701539274e57b726c214dcbe9ed1976015
catalin1998 <catalinmorar98@yahoo.com>
2020-02-26 14:18:17 +0200

    Added App2

--- App2/src/main/java/ro/fortech/App2/App2Application.java
+++ App2/src/main/java/ro/fortech/App2/App2Application.java
@@ -2,9 +2,15 @@ package ro.fortech.App2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class App2Application {
+
+	@GetMapping("/app2")
+	String getApp1(){return "Greetings App2";}
 
 	public static void main(String[] args) {
 		SpringApplication.run(App2Application.class, args);
